cd2102ac8ba026cf2c82cd55c65626debcabd873
AALXX <alexn.serban@gmail.com>
2025-07-11 01:03:43 +0300

    docker  deployment client-web

--- client-web/next.config.ts
+++ client-web/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: 'http',
-                hostname: fileServerHost,
+                hostname: 'localhost',
                 port: '5600'
             },
             {
