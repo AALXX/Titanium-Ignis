844ee53b128cc1270a3209620d018546b47e3101
AALXX <alexn.serban@gmail.com>
2025-07-30 20:59:50 +0300

    solved cors error in project-tasks

--- server/project-tasks/src/server.ts
+++ server/project-tasks/src/server.ts
@@ -12,7 +12,7 @@ const pool = createPool()
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ['http://localhost:3000']
+        origin: ['http://localhost:3000', process.env.FRONTEND_URL as string]
     }
 })
 
